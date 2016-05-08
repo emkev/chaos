3e7c6f6e4c10348b37ffba9ca8bca35f4bf8c916
Joseph <emkev@users.noreply.github.com>
2016-05-08 23:21:48 +0800

    Update gsw.c

--- gsw.c
+++ gsw.c
@@ -6,9 +6,28 @@ simulating a ecosystem . Maybe it can be called chaos theory . Who knows !
 #include <stdio.h>
 #include <stdlib.h>
 
-int height = 4 , width = 4 ;
-int plants = 4 ;
+int height = 5 , width = 10 , steps = 3 ;
+int plants = 10 , herbs = 4 , carns = 2 ;
 
+/* energy values */
+int ep = 15 , eh = 50 , ec = 200 ;
+/* energy cost range of herbs and carns , and energy boundary of plants . */
+int pt = 0 , ch = 5 , cc = 5 ;
+/* plants growing range values */
+int pmin = 2 , pmax = 4 ;
+
+/* whether allow extinctions . 
+   I think it should allow extinctions in normal case .
+ */
+int noext = 0 ;
+
+#define EMPTY 0
+#define PLANT 1
+#define HERB  2
+#define CARN  3
+#define NEWPLANT 4
+
+/* each unit in world */
 typedef struct cell {
   unsigned type : 4 ;
   unsigned mark : 4 ;
@@ -30,7 +49,7 @@ int random_range(int min , int max)
 void init_world(void)
 {
   int  i , j , k ;
-  int count ;
+  int count , threshold ;
 
   world = malloc(sizeof(cell *) * height) ;
   for(i = 0 ; i < 4 ; i++)
@@ -38,15 +57,22 @@ void init_world(void)
     world[i] = malloc(sizeof(cell) * width) ;
     for(j = 0 ; j < 4 ; j++)
     {
-      world[i][j].type = 0;
+      world[i][j].type = EMPTY;
       world[i][j].mark = 0;
       world[i][j].energy = 0;
     }
   }
 
-  
+  /* ready a threshold(20%) to limit times active 
+     to find out " empty " .
+     It means that finding 20% chances in entity area .
+     It also can be set to three threshold for plants , herbs , and carns . 
+   */
+  threshold = (int)(height * width / 5) ;
+
   for(i = 0 ; i < plants ; i++)
   {
+    printf("i = %d\n" , i);
     count = 0 ;
     do
     {
@@ -56,10 +82,14 @@ void init_world(void)
       printf("k = %d\n" , k);
       printf("count = %d\n" , count);
       count++ ;
-    } while((world[j][k].type != 0) && (count < 4)) ;
-    world[j][k].type = 1 ;
-    world[j][k].energy = 1 ;    
-  }
+    } while((world[j][k].type != EMPTY) && (count < threshold)) ;
+
+    if(world[j][k].type == EMPTY)
+    {
+      world[j][k].type = PLANT ;
+      world[j][k].energy = 1 ;
+    }
+  } /* for(i = 0 ; i < plants ; i++) */
 
 }
 
